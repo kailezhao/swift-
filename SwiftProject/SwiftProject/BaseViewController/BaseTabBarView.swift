993146b12f2efab7dd8de82350156b6e97738c5c
zkl <zkl@dijiaaotuman.local>
2020-05-15 19:26:07 +0800

    second commit

---
+++ SwiftProject/SwiftProject/BaseViewController/BaseTabBarView.swift
@@ -0,0 +1,21 @@
+//
+//  BaseTabBarView.swift
+//  SwiftProject
+//
+//  Created by zkl on 2020/5/15.
+//  Copyright © 2020 zkl. All rights reserved.
+//
+
+import UIKit
+
+class BaseTabBarView: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SwiftProject/SwiftProject/GlobalFile/GlobalHeader.swift
@@ -0,0 +1,9 @@
+//
+//  GlobalHeader.swift
+//  SwiftProject
+//
+//  Created by zkl on 2020/5/15.
+//  Copyright © 2020 zkl. All rights reserved.
+//
+
+import Foundation
